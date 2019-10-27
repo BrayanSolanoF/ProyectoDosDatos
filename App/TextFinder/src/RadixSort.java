8f9f94f032dfd9574ce235f8c4fff70c130bd9cf
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:22:31 -0600

    Update
    Update RadixSort

--- App/TextFinder/src/RadixSort.java
+++ App/TextFinder/src/RadixSort.java
@@ -36,5 +36,17 @@ public class RadixSort {
         for (i = 0; i < n; i++)
             arr[i] = output[i];
     }
+    public static int[] radixsort(int arr[], int n) {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10){
+            countSort(arr, n, exp);
+        }
+        return arr;
+    }
+
 
 }
