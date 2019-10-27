2be9ad403710327102ac945cc6472e1e565cb784
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:28:58 -0600

    Update
    Metodos de clase Documents
    + Patron de Diseno

--- App/TextFinder/src/Documents.java
+++ App/TextFinder/src/Documents.java
@@ -12,4 +12,36 @@ public class Documents {
     private String date;
     private int realSize;
 
+    protected Documents next = null;
+    protected Documents prev = null;
+
+    public Documents(String text, String name, String size, String date) {
+        this.text = text;
+        this.name = name;
+        this.size = size;
+        this.date = date;
+        this.realSize = toInt(this.size);
+    }
+
+    public static int toInt(String size){
+        String subSize[] = size.split(" ");
+        int result = 0;
+        int exp = 0;
+        for(int i = subSize.length-1; i == 0; i--, exp++){
+            switch (subSize[i]){
+                case "1": {result += Math.pow(1, exp);}
+                case "2": {result += Math.pow(2, exp);}
+                case "3": {result += Math.pow(3, exp);}
+                case "4": {result += Math.pow(4, exp);}
+                case "5": {result += Math.pow(5, exp);}
+                case "6": {result += Math.pow(6, exp);}
+                case "7": {result += Math.pow(7, exp);}
+                case "8": {result += Math.pow(8, exp);}
+                case "9": {result += Math.pow(9, exp);}
+            }
+        }
+        return result;
+    }
+
+
 }
