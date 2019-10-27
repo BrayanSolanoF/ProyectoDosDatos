e43ab4ce55dac58ded1fd4935a1d60a4702a6f7a
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:11:52 -0600

    Update
    LibreriasPDF

--- App/TextFinder/src/PdfReader.java
+++ App/TextFinder/src/PdfReader.java
@@ -1,5 +1,9 @@
 
-
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+import java.io.File;
+import java.io.IOException;
 /**
  * Esta clase permite leer el contenido de archivos de formato .pdf
  *
