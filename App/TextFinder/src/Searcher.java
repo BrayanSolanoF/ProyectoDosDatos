4e6bf5ec380b9e696820c642dd06a515fce9e33d
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 07:09:51 -0600

    Update Searcher
    Metodo para bsucar la frase o palabra ingresada por el usuario

--- App/TextFinder/src/Searcher.java
+++ App/TextFinder/src/Searcher.java
@@ -11,5 +11,39 @@ public class Searcher {
         this.controller=controller;
     }
 
+    public void search(String phrase){
+        String[] splitPhrase = this.decomposePhrase(phrase);
+        if(splitPhrase.length>=1){
+
+            WordOcurrences wordOcurrences= this.searchWord(splitPhrase[0]);
+            if( wordOcurrences!= null) {
+                wordOcurrences = this.getRealOcurrences(wordOcurrences, splitPhrase);
+
+                String[] context = this.getContext(wordOcurrences);
+                String[] names = this.getNames(wordOcurrences.getDocuments());
+
+                String[] dates;
+                try {
+                    dates = this.getDates(wordOcurrences.getDocuments());
+                } catch (IOException e) {
+                    dates = null;
+                }
+
+                String[] sizes;
+                try {
+                    sizes = this.getSizes(wordOcurrences.getDocuments());
+                } catch (IOException e) {
+                    sizes = null;
+                }
+                controller.updateSearchPane(wordOcurrences.getDocuments(), context, names, dates, sizes);
+            }else{
+                AlertBoxes.displayAlertBox("Error", "No encontrado");
+            }
+        }else{
+            AlertBoxes.displayAlertBox("Error", "No se ingreso palabras");
+        }
+    }
+
+
 
 }
