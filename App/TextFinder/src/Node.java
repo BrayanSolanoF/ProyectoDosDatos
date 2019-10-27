fd6d43badb40faf23a24ecce3e26453a56ee1772
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:40:24 -0600

    Update

--- App/TextFinder/src/Node.java
+++ App/TextFinder/src/Node.java
@@ -5,6 +5,23 @@ public class Node {
 
     private Node rigth;
 
+    private WordOcurrences wordOcurrences;
+
+    private String word;
+
+    public Node(String word){
+        this.word=word;
+        wordOcurrences=new WordOcurrences();
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
 
 }
 
