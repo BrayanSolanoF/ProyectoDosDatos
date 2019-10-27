4fb589680a309fe748163e704f50b0726903c015
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:18:14 -0600

    .
    Ventana Interfaz

--- App/TextFinder/src/Main.java
+++ App/TextFinder/src/Main.java
@@ -1,9 +1,28 @@
 
-public class Main  {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import java.io.IOException;
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Text Finder");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+    }
 
 
     public static void main(String[] args) {
+        launch(args);
     }
 }
 
