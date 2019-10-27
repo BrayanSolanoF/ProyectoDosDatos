ed7af67264170cff4bf6d0a07b890a2ebc567389
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:03:10 -0600

    Update
    PatrondeDiseno

--- App/TextFinder/src/ParserFacade.java
+++ App/TextFinder/src/ParserFacade.java
@@ -2,6 +2,32 @@
 
 public class ParserFacade {
 
+    public static String[][] parse(File file) throws  IOException {
+        String extension = getExtension(file);
+        String[] lines;
+        String[][] content;
+        switch (extension){
+            case "pdf":
+                lines= PdfReader.pdfReader(file.getPath());
+                content= toBidemensionalArray(lines);
+                updateTree(file,content);
+                return content;
+            case "txt":
+                lines= TxtReader.txtReader(file.getPath());
+                content= toBidemensionalArray(lines);
+                updateTree(file,content);
+                return content;
+            case "docx":
+                lines= DocxReader.docxReader(file.getPath());
+                content= toBidemensionalArray(lines);
+                updateTree(file,content);
+                return content;
+            default:
+                return null;
+        }
+
+    }
+
 
 }
 
