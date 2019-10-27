2295ba5b6145b734c0e481c81ddc707969f6dbcf
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:25:37 -0600

    Update
    Clase LinkedList finalizada

--- App/TextFinder/src/Mi_Lista.java
+++ App/TextFinder/src/Mi_Lista.java
@@ -26,6 +26,107 @@ public class Mi_Lista{
         return this.first == null;
     }
 
+    public Documents get(int index){
+        if (index > this.length-1 || this.first == null || index < 0){
+            System.out.println("Index out of range");
+            return null;
+        }
+        else{
+            int counter = 0;
+            Documents current = this.first;
+            while(counter < index){
+                current = current.next;
+                counter ++;
+            }
+            return current;
+        }
+
+        public void deleteAt(int index){
+            if (index > this.length-1 || this.first == null || index < 0){
+                System.out.println("Index out of range");
+            }
+            else{
+                int counter = 0;
+                Documents current = this.first;
+                while(counter < index){
+                    current = current.next;
+                    counter ++;
+                }
+                if (current == this.first){
+                    Documents after = current.next;
+                    after.prev = null;
+                    this.first = after;
+                }
+                else if (current == this.last){
+                    Documents before = current.prev;
+                    before.next = null;
+                    this.last = before;
+                } else {
+                    Documents before = current.prev;
+                    Documents after = current.next;
+                    before.next = after;
+                    after.prev = before;
+
+                }
+            }
+        }
+
+    }
+
+    public void printList(){
+        if (this.first == null){
+            System.out.println("[]");
+        }
+        else {
+            Documents current = this.first;
+            System.out.print("[");
+            while (current.next != null) {
+                System.out.print(current.getName());
+                System.out.print(", ");
+                current = current.next;
+            }
+            System.out.print(current.getName());
+            System.out.println("]");
+        }
+    }
+
+    public void addLast(Documents addedDoc) throws MalformedURLException {
+        this.length ++;
+        if (this.first == null){
+            this.first = addedDoc;
+            this.last = this.first;
+        }
+        else {
+            Documents newDoc = addedDoc;
+            this.last.next = newDoc;
+            newDoc.prev = this.last;
+            this.last = newDoc;
+        }
+    }
+
+    public void reverseList(){
+        Documents previous = null;
+        //change reference head becomes tail in reversal
+        this.last = this.first;
+        Documents current = this.first;
+        while(current != null){
+            // swap prev and next for the current node
+            previous = current.prev;
+            current.prev = current.next;
+            current.next = previous;
+            // to move to next node current.prev has to be called
+            // as that has reference to next node now
+            current = current.prev;
+        }
+        if(previous != null){
+            this.first = previous.prev;
+        }
+    }
+
+    public int getLength() {
+        return length;
+    }
+
 
 
 }
