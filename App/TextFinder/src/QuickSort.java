1b6eb3b0375dbf1d19d779a26ea45a1c39778505
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:14:39 -0600

    Update
    AvanceQuick

--- App/TextFinder/src/QuickSort.java
+++ App/TextFinder/src/QuickSort.java
@@ -1,5 +1,30 @@
 public class QuickSort {
 
+    public static void quickSort(Mi_Lista dl, int beginning, int end){
+        if (dl.isEmpty() || dl.getLength() == 0)
+            AlertBoxes.displayAlertBox("Exception", "Empty library");
+        if (beginning >= end)
+            return;
+
+        Documents pivot = dl.get(beginning + (end - beginning) / 2);
+
+        int i = beginning, j = end;
+        while (i <= j) {
+            while (dl.get(i).getName().compareTo(pivot.getName()) < 0) {
+                i++;
+            }
+
+            while (dl.get(j).getName().compareTo(pivot.getName()) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                swap(dl.get(i), dl.get(j), dl);
+                i++;
+                j--;
+            }
+        }
+
 
 }
 
