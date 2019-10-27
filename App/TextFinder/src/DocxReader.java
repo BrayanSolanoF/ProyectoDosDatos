27b2d0b61d546b49a8ad433206bcdcbd26635e59
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:02:06 -0600

    Update
    DocxReader

--- App/TextFinder/src/DocxReader.java
+++ App/TextFinder/src/DocxReader.java
@@ -1,9 +1,26 @@
-
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 /**
  * Esta clase permite leer el contenido de archivos con formato .docx
  * 
  *
  */
 public class DocxReader {
+
+    public static String[] docxReader(String path) throws IOException {
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+        XWPFDocument document = new XWPFDocument(fis);
+        XWPFWordExtractor extractor= new XWPFWordExtractor(document);
+        String[] lines = extractor.getText().split("\n");
+
+        fis.close();
+        return lines;
+
+    }
 
 }
