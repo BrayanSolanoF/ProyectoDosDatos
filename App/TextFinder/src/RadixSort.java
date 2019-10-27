482e6f8dabdb12699de02819be764a9454cd3386
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:21:26 -0600

    Update
    FuncionParaContar

--- App/TextFinder/src/RadixSort.java
+++ App/TextFinder/src/RadixSort.java
@@ -10,4 +10,31 @@ public class RadixSort {
         return mx;
     }
 
+    public static void countSort(int arr[], int n, int exp) {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Build the output array
+        for (i = n - 1; i >= 0; i--) {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
 }
