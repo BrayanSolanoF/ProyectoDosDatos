a2584083d05cb5f1f87826b9be254eeaa8138e85
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:44:06 -0600

    Update
    Botones

--- App/TextFinder/src/Controller.java
+++ App/TextFinder/src/Controller.java
@@ -2,6 +2,24 @@
 
 public class Controller {
 
+    @FXML
+    Button indexBtn;
+
+
+    @FXML
+    Button refreshBtn;
+
+
+    @FXML
+    Button addBtn;
+
+    @FXML
+    Button deleteBtn;
+
+    @FXML
+    Button searchBtn;
+
+
 
 }
 
