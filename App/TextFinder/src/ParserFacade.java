630e3945a7a296ef489ed440743a803e67d9b137
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:04:35 -0600

    Update
    ArrayBidimensional

--- App/TextFinder/src/ParserFacade.java
+++ App/TextFinder/src/ParserFacade.java
@@ -28,6 +28,28 @@ public class ParserFacade {
 
     }
 
+    private static String[][] toBidemensionalArray(String[] lines){
+        String[][] content = new String[lines.length][1];
+        for(int i =0;i<lines.length;i++) {
+            String phrase= lines[i];
+            String[] split = phrase.split(" ");
+            ArrayList<String> noSpacesSplit = new ArrayList<>();
+
+            for (String word : split) {
+                word=word.replaceAll("\n","");
+                if (!word.equals("")) {
+                    noSpacesSplit.add(word);
+                }
+            }
+
+            split = noSpacesSplit.toArray(split);
+            content[i]=split;
+        }
+
+        return content;
+    }
+
+
 
 }
 
