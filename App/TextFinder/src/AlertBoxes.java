cc3f2ee2f7fefc5b9bc25e34e1174eb777853e0b
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:00:44 -0600

    Update
    AlertBoxes Done

--- App/TextFinder/src/AlertBoxes.java
+++ App/TextFinder/src/AlertBoxes.java
@@ -2,5 +2,19 @@ import javafx.scene.control.Alert;
 
 public class AlertBoxes {
 
+    public static void displayAlertBox(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void displayResultAlertBox(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 
 }
