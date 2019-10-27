ba78f63c865babb1c92908fafd716f747ec2975e
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 07:26:34 -0600

    Update Tree
    Añade una ocurrencia a la palabra, en caso de que la palabra ya exista en el árbol

--- App/TextFinder/src/Tree.java
+++ App/TextFinder/src/Tree.java
@@ -13,5 +13,28 @@ public class Tree {
         root=this.add(word, document,  lineNumber,  linePos, this.root);
     }
 
+    private Node add(String word, File document, int lineNumber, int linePos, Node current){
+        if(current==null){
+            Node node= new Node(word);
+            this.addOcurrence(node.getWordOcurrences(),document,lineNumber,linePos);
+            return node;
+        }else if(current.getWord().equals(word)){
+            this.addOcurrence(current.getWordOcurrences(),document,lineNumber,linePos);
+        }else if(current.getWord().compareTo(word)>0){
+            current.setLeft(this.add(word, document,  lineNumber,  linePos, current.getLeft()));
+        }else{
+            current.setRigth(this.add(word, document,  lineNumber,  linePos, current.getRigth()));
+        }
+        return current;
+
+    }
+
+    private void addOcurrence(WordOcurrences wordOcurrences, File document, int lineNumber, int linePos){
+        wordOcurrences.getDocuments().add(document);
+        wordOcurrences.getLineNumber().add(lineNumber);
+        wordOcurrences.getLinePos().add(linePos);
+    }
+
+
 
 }
