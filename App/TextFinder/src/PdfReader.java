19f250f15d77c69c79b59d6cbd6b7e9295dcf794
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 06:11:22 -0600

    Update
    Leer PDF

--- App/TextFinder/src/PdfReader.java
+++ App/TextFinder/src/PdfReader.java
@@ -6,5 +6,18 @@
  */
 public class PdfReader {
 
+    public static String[] pdfReader (String path) throws IOException {
+        PDDocument document = PDDocument.load(new File(path));
+        if (!document.isEncrypted()) {
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(true);
+            PDFTextStripper tStripper = new PDFTextStripper();
+            String pdfFileInText = tStripper.getText(document);
+            String[] lines = pdfFileInText.split("\\r?\\n");
+            return lines;
+        }
+        return null;
+    }
+
 
 }
