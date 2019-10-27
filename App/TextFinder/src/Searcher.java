b45d7b993748e1a8eb32dafa3224acb09e0bf795
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 07:08:14 -0600

    Update
    Arbol

--- App/TextFinder/src/Searcher.java
+++ App/TextFinder/src/Searcher.java
@@ -2,5 +2,14 @@
 
 public class Searcher {
 
+    Tree tree;
+
+    Controller controller;
+
+    public Searcher(Controller controller){
+        this.tree= Tree.getInstance();
+        this.controller=controller;
+    }
+
 
 }
