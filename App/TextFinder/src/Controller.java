543efce6277f33c04aede4498f648d455b8169eb
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 05:44:52 -0600

    Update
    Paneles

--- App/TextFinder/src/Controller.java
+++ App/TextFinder/src/Controller.java
@@ -19,6 +19,21 @@ public class Controller {
     @FXML
     Button searchBtn;
 
+    @FXML
+    TextField inputField;
+
+    @FXML
+    ListView textPane;
+
+    @FXML
+    ListView namePane;
+
+    @FXML
+    ListView sizePane;
+
+    @FXML
+    ListView datePane;
+
 
 
 }
