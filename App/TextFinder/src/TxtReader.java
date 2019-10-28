19c69b1be3f1742c13942cece135af2808ffada3
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2019-10-27 18:35:56 -0600

    Update
    JavaDocs ClaseTxtReader

--- App/TextFinder/src/TxtReader.java
+++ App/TextFinder/src/TxtReader.java
@@ -3,7 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Esta clase permite leer el contenido de archivos de formato .txt
+ *
+ */
+
 public class TxtReader {
+
+    /**
+     * Retorna un String[] con el texto extraído del documento .txt
+     * @param path
+     * @return String[] texto
+     */
 
     public static String[] txtReader(String path) {
         BufferedReader reader;
